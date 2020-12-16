e92e3cce41db80a46c2bda66ed634eb597cab98f
Mason Phillips <mjphillips2000@gmail.com>
2020-12-16 20:29:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,23 +2,7 @@ class Dog
   
   attr_accessor :name, :breed, :id
   
-  def initialize(name:, 
-    breed:,
-    id:)
+  def initialize(name:, breed:, id:)
     @id, @name, @breed = id, name, breed
   end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 end
